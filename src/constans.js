7339f1fb3659bc99c97880c61d7e6c57307cb4de
lowerrider <nastykoocher@gmail.com>
2023-10-06 18:54:46 +0300

    Updates

--- src/constans.js
+++
@@ -1,6 +0,0 @@
-export const REQUEST_STATUS = {
-  loading: "loading",
-  error: "error",
-  done: "done",
-  idle: "idle",
-};
--- src/redux/jokes/jokes.selector.js
+++
@@ -1,2 +0,0 @@
-export const selectJokseList = (state) => state.jokes.jokesList;
-export const selectRequestStatus = (state) => state.jokes.requestStatus;
